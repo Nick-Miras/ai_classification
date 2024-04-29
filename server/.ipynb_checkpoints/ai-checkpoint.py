d835ea8e08d3d8ab608c89d031340a651d126e60
Nick Anthony Miras <mirasnickanthony@gmail.com>
2024-04-29 20:07:00 +0800

    modify requirements.txt

--- server/.ipynb_checkpoints/ai-checkpoint.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ff23e410381c56e2982a3a0f1704b5315ec510818c8564034e877c2db2b84246
-size 1117
--- server/.ipynb_checkpoints/image_handler-checkpoint.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b1e4ffa7f1ac02f6c4c5f5fd4326c590463565563588503201dac70e7c20fc55
-size 614
